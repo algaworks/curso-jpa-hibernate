737aedbe3f206345ae1eaca00fc0e3e2d5525f6a
Normandes Junior <normandes.junior@algaworks.com>
2013-05-25 11:56:00 -0300

    Removendo objeto do banco de dados

---
+++ Aulas/Removendo objeto do banco de dados/locadora-veiculo/src/com/algaworks/curso/jpa2/locadora/main/RemovendoPrimeiroObjeto.java
@@ -0,0 +1,19 @@
+package com.algaworks.curso.jpa2.locadora.main;
+
+import javax.persistence.EntityManager;
+
+import com.algaworks.curso.jpa2.locadora.modelo.Pessoa;
+import com.algaworks.curso.jpa2.locadora.util.JPAUtil;
+
+public class RemovendoPrimeiroObjeto {
+
+	public static void main(String[] args) {
+		EntityManager em = JPAUtil.getEntityManager();
+		
+		Pessoa pessoa = em.find(Pessoa.class, 1L);
+		em.getTransaction().begin();
+		em.remove(pessoa);
+		em.getTransaction().commit();
+	}
+	
+}
