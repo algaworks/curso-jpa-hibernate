171ed065873d5fdc35a9a0c5b2538e3f79d8f939
Normandes Junior <normandes.junior@algaworks.com>
2013-07-20 10:41:28 -0300

    Trocando projeto exemplo

--- Aulas/Identidade de um objeto - equals() e hashCode()/locadora-veiculo/src/com/algaworks/curso/jpa2/locadora/logica/LogicaFactory.java
+++
@@ -1,9 +0,0 @@
-package com.algaworks.curso.jpa2.locadora.logica;
-
-public class LogicaFactory {
-
-	public static CadastroFabricanteLogica getCadastroFabricanteLogica() {
-		return new CadastroFabricanteLogica();
-	}
-	
-}
