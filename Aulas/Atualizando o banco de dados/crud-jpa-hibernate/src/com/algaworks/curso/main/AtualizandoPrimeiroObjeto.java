4f78afff9aa5bfc5478bc424e2b121304043f51f
Normandes Junior <normandes.junior@algaworks.com>
2013-07-24 20:49:26 -0300

    Atualizando o banco de dados

---
+++ Aulas/Atualizando o banco de dados/crud-jpa-hibernate/src/com/algaworks/curso/main/AtualizandoPrimeiroObjeto.java
@@ -0,0 +1,24 @@
+package com.algaworks.curso.main;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.algaworks.curso.modelo.Cliente;
+
+public class AtualizandoPrimeiroObjeto {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemploPU");
+		EntityManager em = emf.createEntityManager();
+
+		Cliente cliente = em.find(Cliente.class, 1L);
+		em.getTransaction().begin();
+		cliente.setNome("José da Silva Pereira");
+		cliente.setIdade(28);
+		em.getTransaction().commit();
+		
+		System.out.println("Cliente alterado com sucesso!");
+	}
+
+}
