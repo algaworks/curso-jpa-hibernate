5db822b28743c2a52376614dab0db57477ab38cc
Normandes Junior <normandes.junior@algaworks.com>
2013-07-26 21:18:13 -0300

    Importando um projeto do GitHub

---
+++ Aulas/Importando um projeto do GitHub/primeiro-projeto-jsf/src/main/java/com/algaworks/jsf/MeuBean.java
@@ -0,0 +1,24 @@
+package com.algaworks.jsf;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class MeuBean implements Serializable {
+
+	private String nome;
+
+	public void transformar() {
+		System.out.println("adlfjalsdjfkl");
+		this.nome = this.nome.toUpperCase();
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
