6161dcc2fb8b8e7a4f870f7204612dd0d04a8e94
Normandes Junior <normandes.junior@algaworks.com>
2013-08-06 07:33:26 -0300

    Relacionamento Muitos-Para-Um

---
+++ Aulas/Relacionamento Muitos-Para-Um/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/ModeloCarro.java
@@ -0,0 +1,65 @@
+package com.algaworks.curso.jpa2.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class ModeloCarro {
+
+	private Long codigo;
+	private String descricao;
+	private Fabricante fabricante;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@ManyToOne
+	public Fabricante getFabricante() {
+		return fabricante;
+	}
+	public void setFabricante(Fabricante fabricante) {
+		this.fabricante = fabricante;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ModeloCarro other = (ModeloCarro) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
+}
