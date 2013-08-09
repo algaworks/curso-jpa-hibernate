99cc39780616ab911b3ec3fa8effaa76a03acb71
Normandes Junior <normandes.junior@algaworks.com>
2013-08-09 20:56:56 -0300

    Inicializacao tardia e ansiosa

---
+++ Aulas/Inicializacao tardia (Lazy) e ansiosa (Eager)/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/service/CadastroAluguelService.java
@@ -0,0 +1,28 @@
+package com.algaworks.curso.jpa2.service;
+
+import java.io.Serializable;
+
+import javax.inject.Inject;
+
+import com.algaworks.curso.jpa2.dao.AluguelDAO;
+import com.algaworks.curso.jpa2.modelo.Aluguel;
+import com.algaworks.curso.jpa2.util.jpa.Transactional;
+
+public class CadastroAluguelService implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private AluguelDAO aluguelDAO;
+	
+	@Transactional
+	public void salvar(Aluguel aluguel) throws NegocioException {
+		
+		if (aluguel.getCarro() == null) {
+			throw new NegocioException("O carro é obrigatório");
+		}
+		
+		this.aluguelDAO.salvar(aluguel);
+	}
+
+}
