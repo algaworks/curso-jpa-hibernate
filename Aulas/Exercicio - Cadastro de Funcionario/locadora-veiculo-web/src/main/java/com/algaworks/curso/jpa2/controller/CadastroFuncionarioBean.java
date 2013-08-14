f6b868d6877f81ca058deff6c3131292248d784a
Normandes Junior <normandes.junior@algaworks.com>
2013-08-14 08:32:47 -0300

    Exercicio - Cadastro de Funcionario

---
+++ Aulas/Exercicio - Cadastro de Funcionario/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/controller/CadastroFuncionarioBean.java
@@ -0,0 +1,52 @@
+package com.algaworks.curso.jpa2.controller;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.algaworks.curso.jpa2.modelo.Funcionario;
+import com.algaworks.curso.jpa2.service.CadastroFuncionarioService;
+import com.algaworks.curso.jpa2.service.NegocioException;
+import com.algaworks.curso.jpa2.util.jsf.FacesUtil;
+
+@Named
+@ViewScoped
+public class CadastroFuncionarioBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Funcionario funcionario;
+	
+	@Inject
+	private CadastroFuncionarioService cadastroFuncionarioService;
+	
+	@PostConstruct
+	public void inicializar() {
+		this.limpar();
+	}
+	
+	public void salvar() {
+		try {
+			this.cadastroFuncionarioService.salvar(funcionario);
+			FacesUtil.addSuccessMessage("Funcionário salvo com sucesso!");
+		} catch (NegocioException e) {
+			FacesUtil.addErrorMessage(e.getMessage());
+		}
+		
+		this.limpar();
+	}
+	
+	public void limpar() {
+		this.funcionario = new Funcionario();
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+}
---
+++ Aulas/Exercicio - Cadastro de Funcionario/locadora-veiculo-web/src/main/java/com/algaworks/curso/jpa2/modelo/ModeloCarro.java
@@ -0,0 +1,78 @@
+package com.algaworks.curso.jpa2.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class ModeloCarro {
+
+	private Long codigo;
+	private String descricao;
+	private Fabricante fabricante;
+	private Categoria categoria;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public Long getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@ManyToOne
+	@JoinColumn(name="codigo_fabricante")
+	public Fabricante getFabricante() {
+		return fabricante;
+	}
+	public void setFabricante(Fabricante fabricante) {
+		this.fabricante = fabricante;
+	}
+	
+	@Enumerated(EnumType.STRING)
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ModeloCarro other = (ModeloCarro) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
+}
