7f11f4c21b63afbafebe6723af3210fcd0c44dee
Normandes Junior <normandes.junior@algaworks.com>
2013-12-21 08:25:55 -0200

    Consultas nativas

---
+++ Aulas/Consultas nativas/ConsultasNativas.java
@@ -0,0 +1,48 @@
+package com.algaworks.curso.jpa2.criteria;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.algaworks.curso.jpa2.modelo.Carro;
+
+public class ConsultasNativas {
+
+private static EntityManagerFactory factory;
+	
+	private EntityManager manager;
+	
+	@BeforeClass
+	public static void init() {
+		factory = Persistence.createEntityManagerFactory("locadoraVeiculoPU");
+	}
+	
+	@Before
+	public void setUp() {
+		this.manager = factory.createEntityManager();
+	}
+	
+	@After
+	public void tearDown() {
+		this.manager.close();
+	}
+	
+	@Test
+	public void consultaNativa() {
+		Query query = this.manager.createNativeQuery("select * from Carro", Carro.class);
+		List<Carro> carros = query.getResultList();
+		
+		for (Carro carro : carros) {
+			System.out.println(carro.getPlaca());
+		}
+	}
+	
+}
