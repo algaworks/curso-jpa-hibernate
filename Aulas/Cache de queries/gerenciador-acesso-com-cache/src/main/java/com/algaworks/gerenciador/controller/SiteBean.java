b7ce1d33e5acc3c075e262214903a8a6b67bf85f
Normandes Junior <normandesjr@gmail.com>
2014-08-05 07:51:31 -0300

    Cache de queries

---
+++ Aulas/Cache de queries/gerenciador-acesso-com-cache/src/main/java/com/algaworks/gerenciador/controller/SiteBean.java
@@ -0,0 +1,42 @@
+package com.algaworks.gerenciador.controller;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+
+import org.hibernate.Session;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+
+import com.algaworks.gerenciador.model.Status;
+import com.algaworks.gerenciador.model.Usuario;
+
+@Named
+@ViewScoped
+public class SiteBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private EntityManager manager;
+	
+	public Long getTotalUsuarios() {
+		Session session = manager.unwrap(Session.class);
+		return (Long) session.createQuery("select count(u) from Usuario u")
+				.setCacheable(true)
+				.uniqueResult();
+	}
+	
+	public Long getUsuariosAtivos() {
+		Session session = manager.unwrap(Session.class);
+		return (Long) session.createCriteria(Usuario.class)
+				.setProjection(Projections.count("codigo"))
+				.add(Restrictions.eq("status", Status.ATIVO))
+				.setCacheable(true)
+				.uniqueResult();
+	}
+	
+}
